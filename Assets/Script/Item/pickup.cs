d5c0cf543672b81c13a6ab914dffacd82290660e
iover611025 <121559575+iover611025@users.noreply.github.com>
2025-08-12 16:23:04 +0800

    a

--- Assets/Script/Item/pickup.cs
+++ Assets/Script/Item/pickup.cs
@@ -1,22 +1,30 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
-using X;
 
-namespace x
+namespace X
 {
     public class PickupableItem : MonoBehaviour, IPointerClickHandler
     {
-        public int itemIndex;
-
+        public int itemID;               // 只需在Inspector填id
+        public ItemDatabase itemDatabase; // 拖進資料表ScriptableObject
 
         public void OnPointerClick(PointerEventData eventData)
         {
-            // 取得場景中的 InventorySystemUI 物件
-            InventorySystemUI inventory = Object.FindFirstObjectByType<InventorySystemUI>();
-            if (inventory != null)
+            if (itemDatabase == null)
             {
-                inventory.PickItem(itemIndex); // 呼叫背包管理腳本的方法
-                Destroy(gameObject);           // 撿起後讓物品消失
+                Debug.LogWarning("請先將ItemDatabase拖進PickupableItem腳本的itemDatabase欄位！");
+                return;
+            }
+            var data = itemDatabase.items.Find(x => x.id == itemID);
+            if (data != null)
+            {
+                Debug.Log($"你撿到：【{data.itemName}】");
+                // 這裡可以呼叫你的背包系統，把 data 加入背包
+                Destroy(gameObject); // 撿起後物品消失
+            }
+            else
+            {
+                Debug.LogWarning($"找不到ID為{itemID}的道具！");
             }
         }
     }
